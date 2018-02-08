54098d30840c61c578f2176276ee7892518bfe8a
ymzuiku <ymzuiku@gmail.com>
2018-02-09 02:39:43 +0800

    cli

--- config/cli/bin.js
+++ config/cli/bin.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+
 var path = require('path')
 var fse = require('fs-extra')
 var __root = path.resolve(process.argv[1], '../../../')
@@ -6,12 +7,12 @@ var to = path.resolve(process.cwd(), process.argv[2])
 
 let copyFiles = ['app', 'client', 'config', 'test', '.eslintrc', '.gitignore', 'app-package.json', 'README.md']
 
-copyFiles.map(async (v) => {
+copyFiles.map(async (v, i) => {
   await fse.copy(
-    __root+'/'+v,
+    __root + '/' + v,
     path.resolve(to, v))
-  if(v === 'app-package.json'){
-    await fs.move(to+'/app-package.json', to+'/package.json')
+  if (i === copyFiles.length - 1) {
+    await fs.move(to + '/app-package.json', to + '/package.json')
+    console.log('创建成功')
   }
 })
-//
