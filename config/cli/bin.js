2c8463acda000918a44f585b77016bc2aa97129f
ymzuiku <ymzuiku@gmail.com>
2018-02-09 02:33:18 +0800

    change app-package.json

--- config/cli/bin.js
+++ config/cli/bin.js
@@ -4,11 +4,14 @@ var fse = require('fs-extra')
 var __root = path.resolve(process.argv[1], '../../../')
 var to = path.resolve(process.cwd(), process.argv[2])
 
-let copyFiles = ['app', 'client', 'config', 'test', '.eslintrc', '.gitignore', 'package.json', 'README.md']
+let copyFiles = ['app', 'client', 'config', 'test', '.eslintrc', '.gitignore', 'app-package.json', 'README.md']
 
 copyFiles.map(async (v) => {
   await fse.copy(
     __root+'/'+v,
     path.resolve(to, v))
+  if(v === 'app-package.json'){
+    await fs.move(to+'/app-package.json', to+'/package.json')
+  }
 })
 //
