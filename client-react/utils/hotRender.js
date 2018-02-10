4e9624064c0294f06e57875b4f2bd379884244c6
ymzuiku <ymzuiku@gmail.com>
2018-02-11 03:26:09 +0800

    change utils

--- client-react/utils/hotRender.js
+++ client-react/utils/hotRender.js
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import env from './env'
 
-function hotRender(App){
+function hotRender(App, theId='root'){
   if (env.dev) {
     const { hot } = require('react-hot-loader')
     const AppHot = hot(module)(App)
     window.onload = function(){
-      ReactDOM.render(<AppHot />, document.getElementById('root'))
+      ReactDOM.render(<AppHot />, document.getElementById(theId))
     }
   } else {
     window.onload = function(){
-      ReactDOM.render(<App />, document.getElementById('root'))
+      ReactDOM.render(<App />, document.getElementById(theId))
     }
   }
 }
